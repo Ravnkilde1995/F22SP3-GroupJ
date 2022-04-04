8cdf29433d066155cb8ab4bb1038d40baaad6209
NatasjaVitoft <cph-nn194@cphbusiness.dk>
2022-04-04 10:09:28 +0200

    added IOTournament

---
+++ src/Tournament/DataIOTournament.java
@@ -0,0 +1,13 @@
+package Tournament;
+
+public interface DataIOTournament {
+    void saveData();
+    void loadData();
+    void showAllTeams();
+    void getStartTime();
+    void setStartTime();
+    void getEndTime();
+    void setEndTime();
+    void getDate();
+    void setDate();
+}
--- src/Tournament/Tournament.java
+++ src/Tournament/Tournament.java
@@ -1,6 +1,7 @@
 package Tournament;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tournament {
     //Fields
@@ -13,16 +14,15 @@ public class Tournament {
     //Constructor
     //******************
     public Tournament(double startTime,double endTime, String date){
-    this.startTime=startTime;
-    this.endTime=endTime;
-    this.date=date;
+        this.startTime=startTime;
+        this.endTime=endTime;
+        this.date=date;
 
     }
 
     //Methods
     //******************
     public double getStartTime(){
-
         return startTime;
     }
 
@@ -46,15 +46,26 @@ public class Tournament {
         this.date=date;
     }
 
-    public void addGameToTournament(){
-
+    public static void addGameToTournament(Match match) {
+        gamesPlayed.add(match);
     }
 
-    public void createTournament(){
-
+    public static void createTournament(Scanner input, ArrayList<Teams> teams){
+        System.out.println("Type tournament name");
+        String tournamentName = input.next();
+        System.out.println("Type in the date of the tournament");
+        String date = input.next();
+        System.out.println("Type in the start time of the tournament");
+        int startTime = input.nextInt();
+        System.out.println("Type in the end time of the tournament");
+        int endTime = input.nextInt();
+        System.out.println(date + " " + startTime + " " + endTime);
     }
 
-    public void showAllTeams(){
-
+    public static void showAllTeams(ArrayList<Teams> teams){
+        System.out.println("All teams in the tournament:");
+        for (Teams t : teams) {
+            System.out.println(t);
+        }
     }
 }
